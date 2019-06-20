61b0faa938ed3ce9bbde24c7f861174fabaa2ad0
Sam Stevens <SStevens@daytonfreight.com>
2019-06-20 07:52:26 -0400

    fixed width to login screen

--- SrMixAlot-chat-c184f3913e90/ChatWpf/ViewModels/AddChatRoomViewModel.cs
+++ SrMixAlot-chat-c184f3913e90/ChatWpf/ViewModels/AddChatRoomViewModel.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ChatWpf.Models;
+﻿using ChatWpf.Models;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
+using System.Collections.Generic;
 
 namespace ChatWpf.ViewModels
 {
