5cd3867ad0a73827e815a38ed09f3d79ff7b44de
Sam <samstevens.ohio@gmail.com>
2020-09-20 09:25:29 -0400

    removing usings

--- SrMixAlot-chat-c184f3913e90/Shared.Models/Models/ClientMessage.cs
+++ SrMixAlot-chat-c184f3913e90/Shared.Models/Models/ClientMessage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shared.Models.Models
+﻿namespace Shared.Models.Models
 {
     /// <summary>
     ///     Defines a client message.
